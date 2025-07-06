285909c46212b88105efe9bf2bd8a19d229aec69
kayu0514 <kunka6066@gmail.com>
2025-07-07 06:22:01 +0900

    Add new features and increase log levels.

--- VersaLog/versalog.go
+++ VersaLog/versalog.go
@@ -14,21 +14,30 @@ type VersaLog struct {
 }
 
 var COLORS = map[string]string{
-	"INFO":    "\033[32m",
-	"ERROR":   "\033[31m",
-	"WARNING": "\033[33m",
+	"INFO":     "\033[32m",
+	"ERROR":    "\033[31m",
+	"WARNING":  "\033[33m",
+	"DEBUG":    "\033[36m",
+	"CRITICAL": "\033[35m",
 }
 
 var SYMBOLS = map[string]string{
-	"INFO":    "[+]",
-	"ERROR":   "[-]",
-	"WARNING": "[!]",
+	"INFO":     "[+]",
+	"ERROR":    "[-]",
+	"WARNING":  "[!]",
+	"DEBUG":    "[D]",
+	"CRITICAL": "[C]",
 }
 
 const RESET = "\033[0m"
 
 func NewVersaLog(mode string, showFile bool) *VersaLog {
 	mode = strings.ToLower(mode)
+
+	validModes := map[string]bool{"simple": true, "detailed": true, "file": true}
+	if !validModes[mode] {
+		panic(fmt.Sprintf("Invalid mode '%s' specified. Valid modes are: simple, detailed, file", mode))
+	}
 
 	if mode == "file" {
 		showFile = true
@@ -88,10 +97,18 @@ func (v *VersaLog) Info(msg string) {
 	v.log(msg, "INFO")
 }
 
-func (v *VersaLog) Err(msg string) {
+func (v *VersaLog) Error(msg string) {
 	v.log(msg, "ERROR")
 }
 
-func (v *VersaLog) War(msg string) {
+func (v *VersaLog) Warning(msg string) {
 	v.log(msg, "WARNING")
+}
+
+func (v *VersaLog) Debug(msg string) {
+	v.log(msg, "DEBUG")
+}
+
+func (v *VersaLog) Critical(msg string) {
+	v.log(msg, "CRITICAL")
 }
