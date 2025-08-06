69d1bdb62a484b8237491fdd465b4178e99840d7
kayu0514 <kunka6066@gmail.com>
2025-08-06 11:09:38 +0900

    Fix

--- tests/log_save_test.go
+++ tests/log_save_test.go
@@ -3,7 +3,7 @@ package main
 import versalog "github.com/kayu0514/VersaLog.go/VersaLog"
 
 func main() {
-	logger := versalog.NewVersaLog("detailed", false, false, "VersaLog", false, false, true)
+	logger := versalog.NewVersaLog("detailed", false, false, "VersaLog", false, false, true, nil)
 
 	logger.Info("info")
 	logger.Error("error")
